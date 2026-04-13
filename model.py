a95c22691f059d66822a9f157f8490c62e2c1103
amine mezoued <mezouedamine@gmail.com>
2026-04-13 11:09:10 +0200

    changed self.zi added bn

--- model.py
+++ model.py
@@ -97,10 +97,10 @@ class AAE(nn.Module):
         #             )
         #         )))
         # Test self.zi sans batch norm et leaky relu
-        self.zi = self.encoder_fc(
+        self.zi = self.bn_lin(self.encoder_fc(
             features.view(
                 -1, features.size(1) * features.size(2) * features.size(3)
-            )
+            ))
         )
 
         x = self.decoder_fc(self.zi)
