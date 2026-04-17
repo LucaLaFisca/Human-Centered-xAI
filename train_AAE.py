573a4798e11e70b0917961e4af42c405fac661b2
amine mezoued <mezouedamine@gmail.com>
2026-04-17 10:53:37 +0200

    changed treshold high =0.75 and low 0.5

--- train_AAE.py
+++ train_AAE.py
@@ -50,7 +50,7 @@ learn.fit_flat_cos(100, lr=5e-4, pct_start=0.72,
         TrackerCallback(),
         SaveModelCallback(fname=model_file),
         EarlyStoppingCallback(min_delta=1e-4, patience=10),
-        UnfreezeFcCritAdaptative(high_threshold=0.4,low_threshold=0.08),
+        UnfreezeFcCritAdaptative(high_threshold=0.75,low_threshold=0.5),
     ]
 )
 
