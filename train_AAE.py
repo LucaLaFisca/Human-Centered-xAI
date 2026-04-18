38b3dcebbf5a2ebc578de381dea3704c9dfc90dd
amine mezoued <mezouedamine@gmail.com>
2026-04-18 11:29:10 +0200

    Threshold H=0.65 L=0.55

--- train_AAE.py
+++ train_AAE.py
@@ -50,7 +50,7 @@ learn.fit_flat_cos(100, lr=1e-4, pct_start=0.72,
         TrackerCallback(),
         SaveModelCallback(fname=model_file),
         EarlyStoppingCallback(min_delta=1e-4, patience=10),
-        UnfreezeFcCritAdaptative(high_threshold=0.70,low_threshold=0.6),
+        UnfreezeFcCritAdaptative(high_threshold=0.65,low_threshold=0.55),
     ]
 )
 
