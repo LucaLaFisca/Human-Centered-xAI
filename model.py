cb8fdd8a29e7f15ffb5e70ec78b72b8d8e24ae5a
amine mezoued <mezouedamine@gmail.com>
2026-04-21 20:07:25 +0200

    added leaky relu to self.zi

--- model.py
+++ model.py
@@ -104,10 +104,10 @@ class AAE(nn.Module):
         #         )))
         # Test self.zi avec batch norm et sans leaky relu 
         #test sans batch norm self.bn_lin
-        self.zi = self.bn_lin(self.encoder_fc(
+        self.zi = F.leaky_relu(self.bn_lin(self.encoder_fc(
             features.view(
                 -1, features.size(1) * features.size(2) * features.size(3)
-            ))
+            )))
         )
         # print(f"-> Après la couche linéaire (zi) : {self.zi.shape}")
         # x = self.decoder_fc(self.zi)
