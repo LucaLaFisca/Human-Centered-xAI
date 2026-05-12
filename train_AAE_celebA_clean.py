f3485632f8cb74bb20464f047be95c0fb063788f
amine mezoued <mezouedamine@gmail.com>
2026-05-12 21:38:31 +0200

    changed epoch to 50

--- train_AAE_celebA_clean.py
+++ train_AAE_celebA_clean.py
@@ -109,7 +109,7 @@ model_file = 'cat_dog_aae_test'
 #print(f"Learning rate valley : {learnint ag_rate.valley:.6f}")
 print(f"start learn.fit")
 #learn.fit_one_cycle(100, lr_max=1e-3)
-learn.fit(70, lr=5e-5, #0.72
+learn.fit(50, lr=5e-5, #0.72
 
     cbs=[
         #GradientAccumulation(n_acc=128),          # Bs=128 n=4 
