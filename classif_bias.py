239bdefccab7c8319860b228bc7f85bfe8f0f321
amine mezoued <mezouedamine@gmail.com>
2026-05-19 12:01:04 +0200

    changed dataset used

--- classif_bias.py
+++ classif_bias.py
@@ -26,12 +26,21 @@ RUN_NAME = f"celeba_classifier_{timestamp}"
 OUT_DIR = Path(f"results/{RUN_NAME}")
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 
-path_imgs = Path('/home/lucaBA3/Arda/Human-Centered-xAI/celeba_mini_clean/img_align_celeba') 
 
+# Dataset preprocessed
+path_imgs = Path('/home/lucaBA3/Amine/Human-Centered-xAI/celeba_mini_biased/img_align_celeba')
+attr_file = '/home/lucaBA3/Amine/Human-Centered-xAI/celeba_mini_biased/list_attr_celeba.txt'
+partition_file = '/home/lucaBA3/Amine/Human-Centered-xAI/celeba_mini_biased/list_eval_partition.txt'
+
+
+#dataset non biaisé
+# path_imgs = Path('/home/lucaBA3/Arda/Human-Centered-xAI/celeba_mini_clean/img_align_celeba') 
+# partition_file = '/home/lucaBA3/Arda/Human-Centered-xAI/celeba_mini_clean/list_eval_partition.txt'
+# attr_file = '/home/lucaBA3/Arda/Human-Centered-xAI/celeba_mini_clean/list_attr_celeba.txt'
 # ==============================================================================
 # 1. PRÉPARATION DES DONNÉES ET LABELS (CELEBA)
 # ==============================================================================
-attr_file = '/home/lucaBA3/Arda/Human-Centered-xAI/celeba_mini_clean/list_attr_celeba.txt'
+
 df_attr = pd.read_csv(attr_file, sep='\s+', header=1)
 
 attr_dict = {
@@ -42,7 +51,7 @@ attr_dict = {
 def get_celeba_label(img_path):
     return attr_dict.get(img_path.name)
 
-partition_file = '/home/lucaBA3/Arda/Human-Centered-xAI/celeba_mini_clean/list_eval_partition.txt'
+
 df_partition = pd.read_csv(partition_file, sep='\s+', header=None, names=['image_id', 'partition'])
 part_dict = dict(zip(df_partition['image_id'], df_partition['partition']))
 
@@ -54,50 +63,50 @@ def celeba_splitter(items):
         elif part == 1: valid_idx.append(i)
     return train_idx, valid_idx
 
-# ==============================================================================
-# 2. INJECTION DU FILTRE ROUGE BIAISÉ ET SAUVEGARDE VISUELLE
-# ==============================================================================
+# # ==============================================================================
+# # 2. INJECTION DU FILTRE ROUGE BIAISÉ ET SAUVEGARDE VISUELLE
+# # ==============================================================================
 
-def get_biased_image(img_path):
-    """Charge l'image et remplace le canal rouge selon le label."""
-    img = Image.open(img_path).convert('RGB')
-    img_np = np.array(img)
+# def get_biased_image(img_path):
+#     """Charge l'image et remplace le canal rouge selon le label."""
+#     img = Image.open(img_path).convert('RGB')
+#     img_np = np.array(img)
     
-    label = get_celeba_label(img_path) 
-    h, w = img_np.shape[0], img_np.shape[1]
+#     label = get_celeba_label(img_path) 
+#     h, w = img_np.shape[0], img_np.shape[1]
     
-    # Génération du bruit aléatoire selon la classe
-    if label == TARGET_ATTRIBUTE:  # Si c'est 'Male'
-        noise = np.random.randint(128, 256, size=(h, w), dtype=np.uint8)
-    else:                          # Si c'est 'Not Male'
-        noise = np.random.randint(0, 128, size=(h, w), dtype=np.uint8)
+#     # Génération du bruit aléatoire selon la classe
+#     if label == TARGET_ATTRIBUTE:  # Si c'est 'Male'
+#         noise = np.random.randint(128, 256, size=(h, w), dtype=np.uint8)
+#     else:                          # Si c'est 'Not Male'
+#         noise = np.random.randint(0, 128, size=(h, w), dtype=np.uint8)
         
-    # Remplacement du canal rouge
-    img_np[:, :, 0] = noise
-    return PILImage.create(img_np)
+#     # Remplacement du canal rouge
+#     img_np[:, :, 0] = noise
+#     return PILImage.create(img_np)
 
-# --- SCRIPT DE VÉRIFICATION (Sauvegarde en PNG au lieu d'afficher) ---
-print("Génération de l'image de vérification du filtre...")
-toutes_les_images = get_image_files(path_imgs)
-images_test = random.sample(list(toutes_les_images), 4)
+# # --- SCRIPT DE VÉRIFICATION (Sauvegarde en PNG au lieu d'afficher) ---
+# print("Génération de l'image de vérification du filtre...")
+# toutes_les_images = get_image_files(path_imgs)
+# images_test = random.sample(list(toutes_les_images), 4)
 
-fig, axes = plt.subplots(4, 2, figsize=(8, 12))
-fig.suptitle("Vérification du Filtre Rouge sur CelebA", fontsize=16)
+# fig, axes = plt.subplots(4, 2, figsize=(8, 12))
+# fig.suptitle("Vérification du Filtre Rouge sur CelebA", fontsize=16)
 
-for i, img_path in enumerate(images_test):
-    img_orig = Image.open(img_path)
-    label = get_celeba_label(img_path)
-    img_biased = get_biased_image(img_path)
+# for i, img_path in enumerate(images_test):
+#     img_orig = Image.open(img_path)
+#     label = get_celeba_label(img_path)
+#     img_biased = get_biased_image(img_path)
     
-    axes[i, 0].imshow(img_orig)
-    axes[i, 0].set_title(f"Originale ({label})")
-    axes[i, 0].axis('off')
+#     axes[i, 0].imshow(img_orig)
+#     axes[i, 0].set_title(f"Originale ({label})")
+#     axes[i, 0].axis('off')
     
-    axes[i, 1].imshow(img_biased)
-    axes[i, 1].set_title(f"Biaisée (Rouge {'Fort' if label==TARGET_ATTRIBUTE else 'Faible'})")
-    axes[i, 1].axis('off')
+#     axes[i, 1].imshow(img_biased)
+#     axes[i, 1].set_title(f"Biaisée (Rouge {'Fort' if label==TARGET_ATTRIBUTE else 'Faible'})")
+#     axes[i, 1].axis('off')
 
-plt.tight_layout()
+# plt.tight_layout()
 
 # Sauvegarde de l'image dans le dossier de résultats du run courant
 chemin_image_verif = OUT_DIR / "verification_filtre_rouge.png"
@@ -110,8 +119,7 @@ print("Lancement de la préparation des données...")
 # --- Création du DataBlock avec le filtre (get_x) ---
 dblock_classif = DataBlock(
     blocks=(ImageBlock, CategoryBlock), 
-    get_items=get_image_files,
-    get_x=get_biased_image,       
+    get_items=get_image_files,       
     get_y=get_celeba_label,      
     splitter=celeba_splitter,
     item_tfms=Resize(256, method=ResizeMethod.Pad, pad_mode=PadMode.Zeros)
